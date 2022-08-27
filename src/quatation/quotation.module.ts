a04b676c191159d1d598426bd51e83e2accd608c
AkshayDS <tokko-dev@keyvalue.systems>
2022-08-27 19:17:01 +0530

    fix

--- src/quatation/quotation.module.ts
+++ src/quatation/quotation.module.ts
@@ -4,15 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuatationResolver } from './resolvers/quotation.resolver';
 import { Quotation } from './entity/quotation.entity';
 import { QuotationService } from './service/quotation.service';
-import { QuotationRepository } from './repository/quatation.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-    Quotation,
-    QuotationRepository,
-    ]),
-],
+  imports: [TypeOrmModule.forFeature([Quotation])],
   providers: [QuotationService, QuatationResolver],
 })
 export class QuotationModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './service/user.service';
 import { UserResolver } from './resolver/user.resolver';
-import { UserController } from './controller/user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserController],
+
   providers: [UserService, UserResolver],
 })
 export class UserModule {}
